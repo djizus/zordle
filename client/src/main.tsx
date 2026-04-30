0d263bfdf4b4730dacc5e6e7b3de073fd0c03641
djizus <mehrcj@gmail.com>
2026-04-30 16:35:37 +0200

    Use configured chain in Starknet provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,20 +4,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { StarknetConfig, jsonRpcProvider, voyager } from "@starknet-react/core";
-import { sepolia } from "@starknet-react/chains";
+import { mainnet, sepolia } from "@starknet-react/chains";
 
 import { cartridgeConnector } from "./cartridgeConnector";
 import { NFT_NETWORK } from "./networkConfig";
 import App from "./App";
 import "./style.css";
 
+const defaultChain = NFT_NETWORK.chainId === "SN_MAIN" ? mainnet : sepolia;
+
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
     <StarknetConfig
       autoConnect
-      chains={[sepolia]}
+      chains={[defaultChain]}
       connectors={[cartridgeConnector]}
-      defaultChainId={sepolia.id}
+      defaultChainId={defaultChain.id}
       explorer={voyager}
       provider={jsonRpcProvider({ rpc: () => ({ nodeUrl: NFT_NETWORK.rpcUrl }) })}
     >
